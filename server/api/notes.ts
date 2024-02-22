30fae02ba0a9797a7db8e20b82a17b9d1e802363
Matt <matttalves@gmail.com>
2024-02-22 10:38:23 -0300

    feat: prisma and notes endpoint

---
+++ server/api/notes.ts
@@ -0,0 +1,10 @@
+import { serverSupabaseUser } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+    const user = await serverSupabaseUser(event)
+    if (!user) {
+        throw createError({ statusCode: 401, message: 'Unauthorized' })
+    }
+
+    return { user }
+})
